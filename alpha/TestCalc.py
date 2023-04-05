1d375b818acb57221df9b49738950931d57fb8bf
Lisa Yi <yeefangxu@gmail.com>
2023-04-04 22:25:42 -0400

    add multiprocessing

--- alpha/TestCalc.py
+++ alpha/TestCalc.py
@@ -1,9 +1,11 @@
-import pandas as pd
-
 from dataloader import DataLoader
+from dataprocessor import DataProcessor
+from settings import FileOrgStructure, AlphaOutputPath
+import pandas as pd
 
 
 class TestAlphaCalc:
+    output_path = AlphaOutputPath
     def __init__(self, alpha_name, universe, reference_data, parameter):
         self.alpha_name = alpha_name
         self.universe = universe
@@ -16,4 +18,6 @@ class TestAlphaCalc:
         adj_factor = self.data_loader.get_adjustment_factor(date, 5, trade_dates)
         prices = self.dataloader.load_processed(date, "1min_PV", 5, trade_dates)
 
+        result = pd.DataFrame()
         print(f"calculating {self.name} with parameter {self.parameter}")
+        DataProcessor.write_data(date, result, FileOrgStructure.DATECOLUMN, TestAlphaCalc.output_path, self.alpha_name)
--- alpha_calculator.py
+++ alpha_calculator.py
@@ -1,8 +1,8 @@
-import logging
-import alpha
-from dataprocessor import DataProcessor
 from dataloader import DataLoader
-from settings import FileOrgStructure, AlphaOutputPath
+import alpha
+import logging
+import multiprocessing
+import time
 
 logger = logging.getLogger(__name__)
 
@@ -10,7 +10,6 @@ logger = logging.getLogger(__name__)
 class AlphaCalculator:
     calc_start = "20180101"
     calc_end = "20201231"
-    output_path = AlphaOutputPath
     class_name_string = "classname"
     universe_string = "universe"
     parameters_string = "parameters"
@@ -18,12 +17,10 @@ class AlphaCalculator:
     @classmethod
     def alpha_calc(cls, cfg_dict, reference_data):
 
-        logger.info("start calculating alpha...")
-
         trade_dates = DataLoader.get_trade_date_between(reference_data, int(cls.calc_start), int(cls.calc_end))
 
         for key, value in cfg_dict.items():
-
+            t = time.perf_counter()
             alpha_name = key
             class_name = value[cls.class_name_string]
             universe = value[cls.universe_string]
@@ -32,9 +29,10 @@ class AlphaCalculator:
             module = __import__(alpha)
             class_ = getattr(module, class_name)
             instance = class_(alpha_name, universe, reference_data, **parameters)
-            for i in range(0, len(trade_dates)):
-                date = trade_dates[i]
-                result = instance.calculate(date)
-                DataProcessor.write_data(date, result, FileOrgStructure.DATECOLUMN, cls.output_path, instance.name)
 
-        logger.info("done calculating alpha.")
+            pool = multiprocessing.Pool(16)
+            pool.map(instance.calculate, trade_dates)
+            pool.close()
+            logger.info(f"calculated alpha in {time.perf_counter() - t} seconds")
+
+        logger.info("done calculating all alpha.")
