39acde136aa4e7db2b12f3c344e79c792a1f9af5
Lisa Yi <yeefangxu@gmail.com>
2023-04-06 21:27:34 -0400

    fix calculate function

--- alpha/TestAlpha.py
+++ alpha/TestAlpha.py
@@ -12,11 +12,12 @@ class TestAlphaCalc:
         self.reference_data = reference_data
         self.data_loader = DataLoader(reference_data)
 
-    def calculate(self, date, trade_dates):
+    def calculate(self, date):
+        trade_dates = self.data_loader.get_all_trade_dates()
         current_universe = self.data_loader.get_current_universe(self, date, self.universe)
         adj_factor = self.data_loader.get_adjustment_factor(date, 5, trade_dates)
         prices = self.dataloader.load_processed(date, "1min_PV", 5, trade_dates)
 
         result = pd.DataFrame()
-        print(f"calculating {self.name} with parameter {self.parameter}")
+        print(f"calculating {self.name} with parameter {self.parameter} for {date}")
         DataProcessor.write_alpha_data(date, result, self.alpha_name + "_" + self.universe)
