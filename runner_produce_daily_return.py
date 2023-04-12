4b68534c72b94c20709ab5773b7d1237021895d1
Lisa Yi <yeefangxu@gmail.com>
2023-04-11 22:25:18 -0400

    add adjf to return calc

--- runner_produce_daily_return.py
+++ runner_produce_daily_return.py
@@ -16,10 +16,12 @@ if __name__ == '__main__':
     referenceData = RawDataLoader.load_all_reference_data()
     dataLoader = DataLoader(referenceData)
     all_dates_to_process = dataLoader.get_trade_date_between(calc_start, calc_end)
-    processed_daily = dataLoader.load_processed_window_list("pv_1min_standard", all_dates_to_process, ["code", "close"])
+    processed_daily = dataLoader.load_processed_window_list("pv_1min_standard", all_dates_to_process, ["code", "close", "cum_adjf"])
 
     # calculate 1min return
     processed_daily = processed_daily.pivot_table(index="date", columns="code", values="close")
+    processed_daily_adjf = processed_daily.pivot_table(index="date", columns="code", values="cum_adjf")
+    processed_daily = processed_daily * processed_daily_adjf
     processed_daily = processed_daily.diff() / processed_daily.shift()
     processed_daily = processed_daily.unstack().rename("return").reset_index().dropna()
 
